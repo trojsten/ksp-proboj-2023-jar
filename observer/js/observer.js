2213db572c8992d3504a9d0086f4d0bc002d9d81
Adam Zahradník <adam@zahradnik.xyz>
2023-03-16 10:35:20 +0100

    add observer controls

--- observer/js/observer.js
+++ observer/js/observer.js
@@ -1,3 +1,19 @@
 const renderer = new Renderer()
 const game = new Game(renderer);
 // game.loadUrl("observer.dat")
+
+document.getElementById("js-speed").addEventListener("change", (e) => {
+    game.renderer.frameSpeed = parseInt(e.target.value)
+})
+
+document.getElementById("js-slider").addEventListener("input", (e) => {
+    game.nextFrameId = parseInt(e.target.value)
+})
+
+document.getElementById("js-play").addEventListener("click", (e) => {
+    game.startPlayback()
+})
+
+document.getElementById("js-pause").addEventListener("click", (e) => {
+    game.stopPlayback()
+})
